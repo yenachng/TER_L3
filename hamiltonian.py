f269a174f6d89cc4d9900f95bf98fce8528085fd
yenachng <yenachng02@gmail.com>
2025-04-02 21:57:30 +0200

    cleaned up redundant files

--- hamiltonian.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-import numpy as np
-import cluster_refinement as clrf
-
-def cl_is_complete(G):
-    cl = clrf.bondy_chvatal_closure(G)
-    n = G.degree
-    return cl.number_of_edges() == n*(n-1)/2
-
